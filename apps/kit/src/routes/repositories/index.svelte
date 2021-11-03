16c9c3fde47f1313bbce3b9e1718847a03da81b1
Lee Norris <lee.norris@arcadia.io>
2021-11-03 13:19:03 -0400

    chore: idk

--- apps/kit/src/routes/repositories/index.svelte
+++ apps/kit/src/routes/repositories/index.svelte
@@ -1,7 +1,25 @@
-<script>
-   let repos = [
-       'one', 'two', 'three'
-   ];
+<script context="module" lang="ts">
+import type { Load } from "@sveltejs/kit";
+
+let repos = [
+	'one', 'two', 'three'
+];
+
+const api = async (url, { fetch }) => {
+	const res = await fetch(`http://localhost:7777/${url}.json`);
+
+	if (!res.ok) {
+		const { message } = await res.json();
+		console.warn(url, message);
+	}
+
+	return await res.json();
+};
+
+export const load: Load = async ({ fetch }) => {
+	return { props: {repos: await api('repositories', {fetch}) }};
+};
+
 </script>
 
 {#each repos as k}
