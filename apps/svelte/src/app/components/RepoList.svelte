2acf6bbbb94121220ec26c98788b13a911dd2eb2
icedlee337 <lee.e.norris@gmail.com>
2022-06-25 11:08:13 -0400

    chore: minor update

--- apps/svelte/src/app/components/RepoList.svelte
+++ apps/svelte/src/app/components/RepoList.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
-import { onMount } from "svelte";
 import {ReposApi} from "../../../../../libs/generated-api/src/lib";
 
-let reposPromise;
+const reposPromise = new ReposApi({basePath: 'http://localhost:3333', isJsonMime: () => true}).repositoryControllerList();
 
-reposPromise = new ReposApi({basePath: 'http://localhost:3333', isJsonMime: () => true}).repositoryControllerList();
-onMount(() => {
-  reposPromise = new ReposApi({basePath: 'http://localhost:3333', isJsonMime: () => true}).repositoryControllerList();
-});
 </script>
 
 <main class="hero">
-	<h1>@GITGROK</h1>
-
 {#await reposPromise}
   loading
 {:then repos}
